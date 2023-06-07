d52e68184708c1a72e3dd686298dfbf85258cf8f
Maya <maia.bilous@gmail.com>
2023-06-07 17:35:28 +0300

    second commit

---
+++ app/src/main/java/com/example/cleanarchitecturenoteapp/ui/theme/Theme.kt
@@ -0,0 +1,25 @@
+package com.example.cleanarchitecturenoteapp.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = androidx.compose.ui.graphics.Color.White,
+    background = DarkGray,
+    onBackground = androidx.compose.ui.graphics.Color.White,
+    surface = LightBlue,
+    onSurface = DarkGray
+)
+
+@Composable
+fun CleanArchitectureNoteAppTheme(darkTheme: Boolean = true, content: @Composable() () -> Unit) {
+    MaterialTheme(
+        colors = DarkColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
